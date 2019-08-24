b159aa56e69b82af3386956bc0d4761129f9a53e
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-08-24 15:44:05 -0300

    Add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/tabs_screen.dart';
 
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
